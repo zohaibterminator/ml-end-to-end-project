544ab64939cf4e825b4089d19a78739ff1a80bb6
zohaibterminator <zohaibsaqib803@gmail.com>
2024-11-03 22:43:40 +0500

    updated preprocessing pipeline

--- api.py
+++ api.py
@@ -1,26 +1,23 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
-import pickle
 import pandas as pd
 import numpy as np
-from zohaibsaqibDS7_Green_model_load_save import load_model
+from model_load_save import load_model
+import dill
+
+def load_preprocessing_components():
+    with open("encoder.pkl", "rb") as f:
+        encoder = dill.load(f)
+    with open("scaler.pkl", "rb") as f:
+        scaler = dill.load(f)
+    return encoder, scaler
 
 app = FastAPI()
 
-# Load preprocessing pipeline and model components
-def load_preprocessing_components():
-    with open("encoder.pkl", "rb") as f:
-        encoder = pickle.load(f)
-    with open("feature_selector.pkl", "rb") as f:
-        feature_selector = pickle.load(f)
-    with open("scaler.pkl", "rb") as f:
-        scaler = pickle.load(f)
-    return encoder, feature_selector, scaler
-
 # Load trained model
 model = load_model()
-encoder, feature_selector, scaler = load_preprocessing_components()
+encoder, scaler = load_preprocessing_components()
 
 # Define input schema
 class InferenceData(BaseModel):
@@ -36,22 +33,28 @@ class InferenceData(BaseModel):
     Oldpeak: float
     ST_Slope: str
 
+
 # Health check endpoint
 @app.get("/")
 def read_root():
     return {"message": "Inference API is up and running"}
 
+
 # Helper function for preprocessing
 def preprocess_data(df: pd.DataFrame) -> np.ndarray:
     # Encode categorical variables
-    df = encoder.transform(df)
-    
-    # Select features
-    df = feature_selector.transform(df)
-    
+    encoded = encoder.transform(df[encoder.feature_names_in_])
+    encoded_df = pd.DataFrame(encoded, columns=encoder.get_feature_names_out(), index=df.index)
+
+    # Extracting features
+    df = pd.concat([df.drop(encoder.feature_names_in_, axis=1), encoded_df], axis=1)
+
+    # Combine and scale features
+    df_selected = pd.concat([df[['Oldpeak', 'MaxHR', 'Age']], df[['ExerciseAngina_Y', 'ST_Slope_Flat', 'ST_Slope_Up']]], axis=1) # directly extracted selected features
+
     # Scale features
-    df = scaler.transform(df)
-    
+    df = scaler.transform(df_selected)
+
     return df
 
 # Endpoint for single prediction
@@ -66,12 +69,13 @@ def predict(data: InferenceData):
 
         # Make prediction
         prediction = model.predict(processed_data)
-        
+
         # Return prediction result
         return {"prediction": int(prediction[0])}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error during prediction: {str(e)}")
+
 
 # Endpoint for batch prediction
 @app.post("/batch_predict")
